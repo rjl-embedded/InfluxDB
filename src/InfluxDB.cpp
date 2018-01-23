8d71c11deb9ca21e7dded5ec8ebe88d01f072c60
richardjlyon <richard@richardlyon.net>
2018-01-23 17:10:16 +0000

    return device name

--- src/InfluxDB.cpp
+++ src/InfluxDB.cpp
@@ -77,6 +77,7 @@ void InfluxDB::begin() {
  String InfluxDB::setDeviceName(String deviceName)
  {
    _deviceName = deviceName;
+   return deviceName;
  }
 
  String InfluxDB::setDatabase(String databaseName)
